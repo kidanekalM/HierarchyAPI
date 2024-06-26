91503752e8358c3e9a0eeceded8fe243f981d617
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-06-26 19:38:14 +0300

    added role repository

---
+++ HierarchyAPI/HierarchyAPI/Models/IRoleRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HierarchyAPI.Models
+{
+    public interface IRoleRepository
+    {
+        public Task<Role> Insert(Role role);
+        public Task<Role> GetSingle(Guid roleId);
+        public Task<Role> Update(Guid guid,Role role);
+        public Task<List<Role>> GetAllRoles();
+        public Task<List<Role>> GetAllChildren(Guid roleId);
+        public Task<Role> Remove(Guid roleId);
+    }
+}
---
+++ HierarchyAPI/HierarchyAPI/Models/RoleRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HierarchyAPI.Models
+{
+    public class RoleRepository:IRoleRepository
+    {
+        private readonly OrgaContext _OrgaContext;
+        public RoleRepository( OrgaContext orgaContext ) 
+        {
+            _OrgaContext = orgaContext;
+        }
+        public async Task<Role> Insert(Role role)
+        {
+            _OrgaContext.roles.Add( role );
+            _OrgaContext.SaveChanges();
+            return role;
+        }
+        public async Task<Role> Remove(Guid roleId)
+        {
+            var role = _OrgaContext.roles.FirstOrDefault<Role>(r => r.Id.Equals(roleId));
+            _OrgaContext.roles.Remove(role) ;
+            _OrgaContext.SaveChanges();
+            return role;
+        }
+
+        public async Task<Role> Update(Guid roleId, Role role)
+        {
+            var oldRole = _OrgaContext.roles.FirstOrDefault<Role>(r => r.Id.Equals(roleId));
+            oldRole.Description = role.Description;
+            oldRole.Name = role.Name;
+            oldRole.Parent = _OrgaContext.roles.FirstOrDefault<Role>(r => r.Id.Equals(oldRole.ParentId));
+            oldRole.ParentId = role.ParentId;
+            _OrgaContext.roles.Update( role);
+            _OrgaContext.SaveChanges();
+            return role;
+        }
+        public async Task<List<Role>> GetAllChildren(Guid roleId)
+        {
+            return _OrgaContext.roles.Where(r=>r.ParentId.Equals(roleId)).ToList();
+        }
+
+        public async Task<List<Role>> GetAllRoles()
+        {
+            return _OrgaContext.roles.ToList();
+        }
+
+        public async Task<Role> GetSingle(Guid roleId)
+        {
+            return _OrgaContext.roles.FirstOrDefault();
+        }
+
+
+    }
+}
