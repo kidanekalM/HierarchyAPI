d5e8c9660ad0f1030808954b4b71b397e1e793e9
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-07-10 13:22:01 +0300

    added dappercontext

---
+++ HierarchyAPI/HierarchyAPI/Models/DapperContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using Npgsql;
+using System.Data;
+
+namespace HierarchyAPI.Models
+{
+    public class DapperContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string ConnectionString;
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            ConnectionString = _configuration.GetConnectionString("Postgres");
+        }
+        public IDbConnection CreateConnection()
+        {
+           return new NpgsqlConnection(ConnectionString); 
+        }
+    }
+}
