d66dd24e8bc0a04ac5f1c232864ece110282f1f3
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-07-11 07:55:31 +0300

    create repository folder

--- HierarchyAPI/HierarchyAPI/Models/Repository/RoleRepository.cs
+++ HierarchyAPI/HierarchyAPI/Models/Repository/RoleRepository.cs
@@ -3,27 +3,27 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
-namespace HierarchyAPI.Models
+namespace HierarchyAPI.Models.Repository
 {
-    public class RoleRepository:IRoleRepository
+    public class RoleRepository : IRoleRepository
     {
         private readonly OrgaContext _OrgaContext;
         private readonly DapperContext _DapperContext;
-        public RoleRepository( OrgaContext orgaContext,DapperContext dapperContext) 
+        public RoleRepository(OrgaContext orgaContext, DapperContext dapperContext)
         {
             _OrgaContext = orgaContext;
             _DapperContext = dapperContext;
         }
         public async Task<Role> Insert(Role role)
         {
-            _OrgaContext.roles.Add( role );
+            _OrgaContext.roles.Add(role);
             _OrgaContext.SaveChanges();
             return role;
         }
         public async Task<Role> Remove(Guid roleId)
         {
-            var role = await _OrgaContext.roles.FirstOrDefaultAsync(r => r.Id==roleId);
-            _OrgaContext.roles.Remove(role) ;
+            var role = await _OrgaContext.roles.FirstOrDefaultAsync(r => r.Id == roleId);
+            _OrgaContext.roles.Remove(role);
             await _OrgaContext.SaveChangesAsync();
             return role;
         }
@@ -34,35 +34,35 @@ namespace HierarchyAPI.Models
             return role;
         }
         [NonAction]
-        public async Task<Role> RemoveRecursiveNonAction(List<Role> Roles,Guid roleId)
+        public async Task<Role> RemoveRecursiveNonAction(List<Role> Roles, Guid roleId)
         {
-            Role toDelte = Roles.Where(r=>r.Id.Equals(roleId)).FirstOrDefault();
-            List<Role> Children =  Roles.Where(r=>r.ParentId.Equals(roleId)).ToList();
-            if (((Children).Count != 0))
+            Role toDelte = Roles.Where(r => r.Id.Equals(roleId)).FirstOrDefault();
+            List<Role> Children = Roles.Where(r => r.ParentId.Equals(roleId)).ToList();
+            if (Children.Count != 0)
             {
                 foreach (var child in Children)
                 {
-                    RemoveRecursiveNonAction(Roles,(Guid)child.Id);
+                    RemoveRecursiveNonAction(Roles, (Guid)child.Id);
                 }
             }
-            var role = Roles.FirstOrDefault<Role>(r => r.Id.Equals(roleId));
+            var role = Roles.FirstOrDefault(r => r.Id.Equals(roleId));
             _OrgaContext.roles.Remove(role);
             return role;
         }
         public async Task<Role> Update(Guid roleId, Role role)
         {
-            var oldRole = _OrgaContext.roles.FirstOrDefault<Role>(r => r.Id.Equals(roleId));
+            var oldRole = _OrgaContext.roles.FirstOrDefault(r => r.Id.Equals(roleId));
             oldRole.Description = role.Description;
             oldRole.Name = role.Name;
-            oldRole.Parent = _OrgaContext.roles.FirstOrDefault<Role>(r => r.Id.Equals(oldRole.ParentId));
+            oldRole.Parent = _OrgaContext.roles.FirstOrDefault(r => r.Id.Equals(oldRole.ParentId));
             oldRole.ParentId = role.ParentId;
-            _OrgaContext.roles.Update( oldRole);
+            _OrgaContext.roles.Update(oldRole);
             _OrgaContext.SaveChanges();
             return role;
         }
         public async Task<List<Role>> GetAllChildren(Guid roleId)
         {
-            return _OrgaContext.roles.Where(r=>r.ParentId.Equals(roleId)).ToList();
+            return _OrgaContext.roles.Where(r => r.ParentId.Equals(roleId)).ToList();
         }
 
         public async Task<List<Role>> GetAllRoles()
@@ -72,7 +72,7 @@ namespace HierarchyAPI.Models
 
         public async Task<Role> GetSingle(Guid roleId)
         {
-            return _OrgaContext.roles.FirstOrDefault(r=>r.Id.Equals(roleId));
+            return _OrgaContext.roles.FirstOrDefault(r => r.Id.Equals(roleId));
         }
         public async Task<IEnumerable<Role>> GetRoles()
         {
--- HierarchyAPI/HierarchyAPI/Program.cs
+++ HierarchyAPI/HierarchyAPI/Program.cs
@@ -1,4 +1,5 @@
 using HierarchyAPI.Models;
+using HierarchyAPI.Models.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
