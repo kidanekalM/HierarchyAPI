9d0a6fdfde69064b67239bbde7883f6a2065d997
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-07-17 17:50:09 +0300

    added getallchildren

---
+++ HierarchyAPI/HierarchyAPI/Models/Queries/GetAllChildrenQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+namespace HierarchyAPI.Models.Queries
+{
+    public class GetAllChildrenQuery:IRequest<List<Role>>
+    {
+        public Guid guid { get; set; }  
+    }
+}
---
+++ HierarchyAPI/HierarchyAPI/Models/Queries/GetAllChildrenQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Core;
+using Dapper;
+using MediatR;
+using System.Reflection.Metadata;
+namespace HierarchyAPI.Models.Queries
+{
+    public class GetAllChildrenQueryHandler:IRequestHandler<GetAllChildrenQuery, List<Role>>
+    {
+        public DapperContext _dapperContext;
+        public GetAllChildrenQueryHandler(DapperContext context)
+        {
+            _dapperContext = context;
+        }
+        public async Task<List<Role>> Handle(GetAllChildrenQuery request,CancellationToken cancellationToken)
+        {
+            var query = "SELECT * FROM public.\"Role_Table\" WHERE \"ParentId\" = @RoleId";
+
+            using (var connection = _dapperContext.CreateConnection())
+            {
+                var children = await connection.QueryAsync<Role>(query, new { RoleId = request.guid});
+                return children.ToList();
+            }
+        }
+    }
+}
