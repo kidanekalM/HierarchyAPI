1b85122374c12f59467129bedec74a2ac0e11cb6
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-07-18 07:45:21 +0300

    Create GetAllRolesQueryHandler.cs

---
+++ HierarchyAPI/HierarchyAPI/Models/Queries/GetAllRolesQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Dapper;
+namespace HierarchyAPI.Models.Queries
+{
+    public class GetAllRolesQueryHandler:IRequestHandler<GetAllRolesQuery,List<Role>>
+    {
+        private readonly DapperContext dapperContext;
+        public GetAllRolesQueryHandler(DapperContext context)
+        {
+            dapperContext = context;
+        }
+        public async Task<List<Role>> Handle(GetAllRolesQuery getAllRolesQuery,CancellationToken cancellationToken)
+        {
+            var query = "SELECT * FROM public.\"Role_Table\"";
+
+            using (var connection = dapperContext.CreateConnection())
+            {
+                var roles = await connection.QueryAsync<Role>(query);
+                return roles.ToList();
+            }
+        }
+    }
+}
