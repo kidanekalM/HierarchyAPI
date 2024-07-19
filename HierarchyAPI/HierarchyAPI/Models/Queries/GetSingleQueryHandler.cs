8220ec4c421cca7e25557ec7138fc5900eac9846
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-07-19 07:45:25 +0300

    Create GetSingleQueryHandler.cs

---
+++ HierarchyAPI/HierarchyAPI/Models/Queries/GetSingleQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Dapper;
+namespace HierarchyAPI.Models.Queries
+{
+    public class GetSingleQueryHandler : IRequestHandler<GetSingleQuery,Role>
+    {
+        private readonly DapperContext _dapperContext;
+        public GetSingleQueryHandler(DapperContext dapperContext) 
+        {
+            _dapperContext = dapperContext;
+        }
+        public async Task<Role> Handle(GetSingleQuery getSingleQuery, CancellationToken cancellationToken)
+        {
+            var query = "SELECT * FROM public.\"Role_Table\" WHERE public.\"Role_Table\".\"Id\" = @RoleId";
+
+            using (var connection = _dapperContext.CreateConnection())
+            {
+                var role = (await connection.QueryAsync<Role>(query, new { RoleId = getSingleQuery.RoleId })).FirstOrDefault();
+                return role;
+            }
+        }
+    }
+}
