70a5c12a90734547412faebfbbb5f3ffcd2af7b0
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-07-21 11:55:27 +0300

    Create InsertCommandHandler.cs

---
+++ HierarchyAPI/HierarchyAPI/Models/Commands/InsertCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Identity.Client;
+using System.Data;
+namespace HierarchyAPI.Models.Commands
+{
+    public class InsertCommandHandler:IRequestHandler<InsertCommand,Role>
+    {
+        private readonly OrgaContext _context;
+        public InsertCommandHandler(OrgaContext orgaContext) 
+        {
+            _context = orgaContext;
+        }
+        public async Task<Role> Handle(InsertCommand insertCommand,CancellationToken cancellationToken)
+        {
+            _context.roles.Add(insertCommand.Role);
+            _context.SaveChanges();
+            return insertCommand.Role;
+        }
+    }
+}
