1602254880929d37eada4735b1bf9e479abf0e23
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-07-26 13:07:06 +0300

    Create UpdateCommandHandler.cs

---
+++ HierarchyAPI/HierarchyAPI/Models/Commands/UpdateCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using System.Data;
+
+namespace HierarchyAPI.Models.Commands
+{
+    public class UpdateCommandHandler : IRequestHandler<UpdateCommand,Role>
+    {
+        private readonly OrgaContext _context;
+        public UpdateCommandHandler(OrgaContext orgaContext) 
+        {
+            _context = orgaContext;
+        }
+        //check null 
+        public async Task<Role> Handle(UpdateCommand command,CancellationToken cancellationToken)
+        {
+            Role role = command.Role;
+            var oldRole = _context.roles.FirstOrDefault(r => r.Id.Equals(command.Id));
+            oldRole.Description = role.Description;
+            oldRole.Name = role.Name;
+            oldRole.Parent = _context.roles.FirstOrDefault(r => r.Id.Equals(role.ParentId));
+            oldRole.ParentId = role.ParentId;
+            _context.roles.Update(oldRole);
+            _context.SaveChanges();
+            return role;
+        }
+    }
+}
