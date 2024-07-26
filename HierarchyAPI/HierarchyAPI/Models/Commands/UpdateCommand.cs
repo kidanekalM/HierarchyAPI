ac9e429373dce6446fc0646403b82ee96ae760b6
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-07-26 13:07:01 +0300

    Create UpdateCommand.cs

---
+++ HierarchyAPI/HierarchyAPI/Models/Commands/UpdateCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+namespace HierarchyAPI.Models.Commands
+{
+    public class UpdateCommand:IRequest<Role>
+    {
+        public Guid Id { get; set; }    
+        public Role Role { get; set; }
+    }
+}
