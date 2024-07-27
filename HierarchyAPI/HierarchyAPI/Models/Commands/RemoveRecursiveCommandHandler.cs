c44a04041c8f3332a0fc8b5710d369db2c5a16ef
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-07-27 08:18:51 +0300

    Update RemoveRecursiveCommandHandler.cs

--- HierarchyAPI/HierarchyAPI/Models/Commands/RemoveRecursiveCommandHandler.cs
+++ HierarchyAPI/HierarchyAPI/Models/Commands/RemoveRecursiveCommandHandler.cs
@@ -5,30 +5,14 @@ namespace HierarchyAPI.Models.Commands
 {
     public class RemoveRecursiveCommandHandler:IRequestHandler<RemoveRecursiveCommand,Role>
     {
-        private readonly OrgaContext _OrgaContext;
-        public RemoveRecursiveCommandHandler(OrgaContext orgaContext)
+        private readonly Repositories.IRoleCommandsRepository _repositoryCommandsRepository;
+        public RemoveRecursiveCommandHandler(Repositories.IRoleCommandsRepository roleCommandsRepository)
         {
-            _OrgaContext = orgaContext;
-        }
-        public async Task<Role> RemoveRecursiveNonAction(List<Role> Roles, Guid roleId)
-        {
-            Role toDelte = Roles.Where(r => r.Id.Equals(roleId)).FirstOrDefault();
-            List<Role> Children = Roles.Where(r => r.ParentId.Equals(roleId)).ToList();
-            if (Children.Count != 0)
-            {
-                foreach (var child in Children)
-                {
-                    RemoveRecursiveNonAction(Roles, (Guid)child.Id);
-                }
-            }
-            var role = Roles.FirstOrDefault(r => r.Id.Equals(roleId));
-            _OrgaContext.roles.Remove(role);
-            return role;
+            _repositoryCommandsRepository = roleCommandsRepository;
         }
         public async Task<Role> Handle(RemoveRecursiveCommand removeRecursiveCommand, CancellationToken cancellationToken)
         {
-            var role = await RemoveRecursiveNonAction(_OrgaContext.roles.ToList(), removeRecursiveCommand.RoleId);
-            _OrgaContext.SaveChanges();
+            var role = await _repositoryCommandsRepository.RemoveRecursive(removeRecursiveCommand.RoleId);
             return role;
         }
     }
