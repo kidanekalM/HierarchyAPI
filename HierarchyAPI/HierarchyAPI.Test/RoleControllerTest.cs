ee0321c08bbe79eba848fa8640389ecc825eff1b
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-07-28 10:06:03 +0300

    Update RoleControllerTest.cs

--- HierarchyAPI/HierarchyAPI.Test/RoleControllerTest.cs
+++ HierarchyAPI/HierarchyAPI.Test/RoleControllerTest.cs
@@ -1,18 +1,27 @@
 using HierarchyAPI.Controllers;
 using HierarchyAPI.Models;
+using HierarchyAPI.Models.Repositories;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using System.Windows.Input;
 
 namespace HierarchyAPI.Test
 {
     public class RoleControllerTest
     {
         RoleController _roleController;
-        Mock<IRoleRepository> _roleRepositoryMock;
+        Mock<IRoleQueryRepository> _roleQueryRepositoryMock;
+        Mock<IRoleCommandsRepository> _rolecmdRepositoryMock;
+        Mock<IMediator> _mediatorMock;
+
+
         public RoleControllerTest()
         {
-            _roleRepositoryMock = new Mock<IRoleRepository>();
-            _roleController = new RoleController(_roleRepositoryMock.Object);
+            _roleQueryRepositoryMock = new Mock<IRoleQueryRepository>();
+            _rolecmdRepositoryMock = new Mock<IRoleCommandsRepository>();
+            _mediatorMock = new Mock<IMediator>();
+            _roleController = new RoleController(_roleQueryRepositoryMock.Object,_rolecmdRepositoryMock.Object,_mediatorMock.Object);
             
         }
         [Fact]
@@ -26,11 +35,15 @@ namespace HierarchyAPI.Test
                 ParentId = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afc4"),
                 Parent = null
             };
-            var result = await _roleController.Insert(newRole);
+            var cmd = new Models.Commands.InsertCommand()
+            {
+                Role = newRole
+            };
+            var result = await _roleController.Insert(cmd);
             Assert.Equal("", "");  
             //Assert.IsType<Task<Role>>(result);
             //Assert.ThrowsAsync<Exception>(() => _roleRepositoryMock.Object.GetSingle((Guid)newRole.Id));
-            var addedRole = await _roleRepositoryMock.Object.GetSingle((Guid)newRole.Id);
+            var addedRole = await _roleQueryRepositoryMock.Object.GetSingle((Guid)newRole.Id);
             //Assert.NotNull(addedRole);
             
             Assert.Equal(newRole.Name, addedRole.Name);
