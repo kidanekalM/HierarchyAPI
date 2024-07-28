21368092230bd946b521264286daed0ce9335f3d
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-07-28 10:06:50 +0300

    Create GetCandidateQueryHandler.cs

---
+++ HierarchyAPI/HierarchyAPI/Models/Queries/GetCandidateQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using HierarchyAPI.Models.Repositories;
+using MediatR;
+
+namespace HierarchyAPI.Models.Queries
+{
+    public class GetCandidateQueryHandler:IRequestHandler<GetCandidatesQuery,List<Role>>
+    {
+        private readonly IRoleQueryRepository _repository;
+        public GetCandidateQueryHandler(IRoleQueryRepository roleQueryRepository)
+        {
+            _repository = roleQueryRepository;
+        }
+        public async Task<List<Role>> Handle (GetCandidatesQuery request,CancellationToken cancellation)
+        {
+            return (await _repository.GetCandidates(request.RoleId)).ToList();
+        }
+    }
+}
