0b8653c4b8962b9f14749bb3506884f019b4fc8c
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-08-02 09:47:57 +0300

    Update Role.cs

--- HierarchyAPI/HierarchyAPI/Models/Role.cs
+++ HierarchyAPI/HierarchyAPI/Models/Role.cs
@@ -5,11 +5,11 @@ namespace HierarchyAPI.Models
     public class Role
     {
         public Guid? Id { get; set; } = Guid.NewGuid();
-        public string? Name { get; set; }
-        public string? Description { get; set; }
+        public string? Role_Name { get; set; }
+        public string? Role_Description { get; set; }
         [ForeignKey("Parent")]
-        public Guid? ParentId { get; set; }
+        public Guid? Parent_Id { get; set; }
         public Role? Parent { get; set; }
-        public bool IsCandidate { get; set; }
+        public bool Is_Candidate { get; set; }
     }
 }
