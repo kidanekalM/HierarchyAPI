ce97810c5bd0c145b294c6a34fe07a9b1d7d6fdd
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-08-03 09:40:40 +0300

    Create RoleQuery.cs

---
+++ HierarchyAPI/HierarchyAPI/Models/Repositories/RoleQuery.cs
@@ -0,0 +1,54 @@
+﻿using Dapper;
+
+namespace HierarchyAPI.Models.Repositories
+{
+    public class RoleQuery:IRoleQueryRepository
+    {
+        private readonly DapperContext _DapperContext;
+        public RoleQuery(DapperContext dapperContext)
+        {
+            _DapperContext = dapperContext;
+        }
+        public async Task<List<Role>> GetAllChildren(Guid roleId)
+        {
+            var query = "SELECT * FROM public.\"role_table\" WHERE \"parent_id\" = @RoleId";
+
+            using (var connection = _DapperContext.CreateConnection())
+            {
+                var children = await connection.QueryAsync<Role>(query, new { RoleId = roleId });
+                return children.ToList();
+            }
+        }
+        public async Task<List<Role>> GetAllRoles()
+        {
+            var query = "SELECT * FROM public.\"role_table\"";
+
+            using (var connection = _DapperContext.CreateConnection())
+            {
+                var roles = await connection.QueryAsync<Role>(query);
+                return roles.ToList();
+            }
+        }
+        public async Task<Role> GetSingle(Guid roleId)
+        {
+            var query = "SELECT * FROM public.\"role_table\" WHERE public.\"role_table\".\"Id\" = @RoleId";
+
+            using (var connection = _DapperContext.CreateConnection())
+            {
+                var role = (await connection.QueryAsync<Role>(query, new { RoleId = roleId })).FirstOrDefault();
+                return role;
+            }
+        }
+        public async Task<List<Role>> GetCandidates(Guid roleId)
+        {
+            var query = "SELECT * FROM public.\"role_table\" WHERE \"parent_id\" = @RoleId AND is_candidate = true";
+
+            using (var connection = _DapperContext.CreateConnection())
+            {
+                var children = await connection.QueryAsync<Role>(query, new { RoleId = roleId });
+                return children.ToList();
+            }
+        }
+
+    }
+}
