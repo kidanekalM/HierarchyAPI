166102894f3e6819e404d5ad1ca7a9910cbf0dc1
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-08-05 10:01:04 +0300

    Create LoggingInterceptor.cs

---
+++ HierarchyAPI/HierarchyAPI/Interceptors/LoggingInterceptor.cs
@@ -0,0 +1,13 @@
+﻿using Castle.DynamicProxy;
+using System.Data.Common;
+
+namespace HierarchyAPI.Interceptors
+{
+    public class LoggingInterceptor:IInterceptor
+    {
+        public void Intercept(IInvocation invocation)
+        {
+            Console.WriteLine(invocation.InvocationTarget.ToString() + " was invoked.");
+        }
+    }
+}
