26bd2d7fa19b795040e33651455d232b53221a42
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-08-07 07:26:54 +0300

    Update Program.cs

--- HierarchyAPI/HierarchyAPI/Program.cs
+++ HierarchyAPI/HierarchyAPI/Program.cs
@@ -1,5 +1,7 @@
+using HierarchyAPI.Interceptors;
 using HierarchyAPI.Models;
 using HierarchyAPI.Models.Repositories;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,8 +13,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly)); builder.Services.AddScoped<IRoleRepository,RoleRepository>();
-builder.Services.AddScoped<IRoleQueryRepository,RoleQueryRepository>();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+//builder.Services.AddScoped<IRoleRepository,RoleRepository>();
+builder.Services.AddScoped<IRoleQueryRepository,RoleQuery>();
 builder.Services.AddScoped<IRoleCommandsRepository,RoleCommandsRepository>();
 builder.Services.AddSingleton<DapperContext>();
 builder.Services.AddDbContext<OrgaContext>(options => {
@@ -21,9 +24,11 @@ builder.Services.AddDbContext<OrgaContext>(options => {
     .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()));
     options.EnableSensitiveDataLogging();
 });
+
 var app = builder.Build();
 
 
+//app.UseMiddleware<LoggingInterceptorMiddleware>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -34,7 +39,6 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
