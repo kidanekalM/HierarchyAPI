e52b7b44c17de12b5271060b69fffbe737c690e6
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-08-09 09:07:40 +0300

    Delete DeleteByCandidateCommand.cs

--- HierarchyAPI/HierarchyAPI/Models/Commands/DeleteByCandidateCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-namespace HierarchyAPI.Models.Commands
-{
-    public class DeleteByCandidateCommand:IRequest<Role>
-    {
-        public Guid Deleted {  get; set; }
-        public Guid Candidate { get; set; }
-    }
-}
