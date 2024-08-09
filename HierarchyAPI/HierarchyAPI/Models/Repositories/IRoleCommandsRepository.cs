aaabbc684c2cef807e7b52f692e826741b78ebcd
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-08-09 13:47:35 +0300

    Update IRoleCommandsRepository.cs

--- HierarchyAPI/HierarchyAPI/Models/Repositories/IRoleCommandsRepository.cs
+++ HierarchyAPI/HierarchyAPI/Models/Repositories/IRoleCommandsRepository.cs
@@ -6,5 +6,8 @@
         public Task<Role> Update(Guid guid, Role role);
         public Task<Role> Remove(Guid roleId);
         public Task<Role> RemoveRecursive(Guid roleId);
+        public Task<Role> GetSingle(Guid roleId);
+        public Task<List<Role>> GetAllRoles();
+        public Task<List<Role>> GetAllChildren(Guid roleId);
     }
 }
