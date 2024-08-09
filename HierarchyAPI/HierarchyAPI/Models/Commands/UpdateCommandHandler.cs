2c79989461f7a0d8be9cad8a3cafc2dbf1f33326
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-08-09 09:32:16 +0300

    Update UpdateCommandHandler.cs

--- HierarchyAPI/HierarchyAPI/Models/Commands/UpdateCommandHandler.cs
+++ HierarchyAPI/HierarchyAPI/Models/Commands/UpdateCommandHandler.cs
@@ -3,6 +3,14 @@ using System.Data;
 
 namespace HierarchyAPI.Models.Commands
 {
+    public class UpdateCommand : IRequest<Role>
+    {
+        public Guid Id { get; set; }
+        public string? Role_Name { get; set; }
+        public string? Role_Description { get; set; }
+        public Guid? Parent_Id { get; set; }
+        public bool Is_Candidate { get; set; }
+    }
     public class UpdateCommandHandler : IRequestHandler<UpdateCommand,Role>
     {
         private readonly Repositories.IRoleCommandsRepository _commandsRepository;
@@ -13,7 +21,16 @@ namespace HierarchyAPI.Models.Commands
         //check null 
         public async Task<Role> Handle(UpdateCommand command,CancellationToken cancellationToken)
         {
-            Role role = await _commandsRepository.Update(command.Id,command.Role);
+
+            var rolToUpdate = new Role()
+            {
+                Id = command.Id,
+                Role_Name = command.Role_Name,
+                Role_Description = command.Role_Description,
+                Parent_Id = command.Parent_Id,
+                Is_Candidate = command.Is_Candidate,
+            };
+            Role role = await _commandsRepository.Update(command.Id,rolToUpdate);
             return role;
         }
     }
