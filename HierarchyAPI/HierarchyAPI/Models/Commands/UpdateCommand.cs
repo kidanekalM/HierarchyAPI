210adfd0339bc5f6fff975dde7ae7b2d944f4df5
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-08-09 09:08:09 +0300

    Delete UpdateCommand.cs

--- HierarchyAPI/HierarchyAPI/Models/Commands/UpdateCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-namespace HierarchyAPI.Models.Commands
-{
-    public class UpdateCommand:IRequest<Role>
-    {
-        public Guid Id { get; set; }    
-        public Role Role { get; set; }
-    }
-}
